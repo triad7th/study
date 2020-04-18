f1a7f1abdc37fa3c185883ef3e8a9d823110a69e
Kevin Lee <wonseok.lee80@gmail.com>
2020-04-18 10:38:55 -0700

    update

--- ti7clock/src/app/app.component.ts
+++ ti7clock/src/app/app.component.ts
@@ -11,7 +11,11 @@ export class AppComponent implements OnInit {
   ) {}
   title = 'ti7clock';
   now: Date;
-
+  flags = {
+    "US": "https://cdn4.iconfinder.com/data/icons/rounded-square-national-flags/512/USA-128.png",
+    'UK': "https://cdn4.iconfinder.com/data/icons/rounded-square-national-flags/512/GREAT_BRITAIN-128.png",
+    'KR': "https://cdn1.iconfinder.com/data/icons/rounded-square-national-flags/512/SOUTH_KOREA-128.png"
+  }
 
   ngOnInit(): void {
     setInterval(() => {
